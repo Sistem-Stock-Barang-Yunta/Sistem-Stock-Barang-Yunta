f7c786ee43055523ab1a3f208120ae7f2dcac9bf
Fajar Angga Maulana <152866536+ellofajar@users.noreply.github.com>
2024-06-11 12:00:07 +0700

    Memperbaiki routing register login

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('frontend.staff.dashboard');
+// });
+// Route untuk menampilkan form login
+Route::get('/', [AuthController::class, 'formlogin'])->name('login');
+
+Route::post('/', [AuthController::class, 'login']);
+
+// Route untuk menampilkan form register
+Route::get('/register', [AuthController::class, 'formregister'])->name('register');
+
+// Route untuk menangani register
+Route::post('/register', [AuthController::class, 'register']);
+
