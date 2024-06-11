f2e9a90f8e531d1c36a4f3effb5a287acbadd104
YusupAbdlAzz <135435597+YusupAbdlAzz@users.noreply.github.com>
2024-06-11 09:35:47 +0700

    create admin CRUD

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/admin', function () {
+    return view('admin.admin');
+});
+
+Route::get('/admin/crud-tambah', function () {
+    return view('admin.create');
+});
+
+Route::get('/admin/crud-edit', function () {
+    return view('admin.edit');
+});
+
+Route::get('/admin', function () {
+    return view('admin.dashboard');
+});
