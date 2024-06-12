daa14de6832309bd25e67df2a2ea440141a3f542
Fajar Angga Maulana <152866536+ellofajar@users.noreply.github.com>
2024-06-12 13:02:09 +0700

    Memperbaiki route

--- routes/web.php
+++ routes/web.php
@@ -3,17 +3,11 @@
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('frontend.staff.dashboard');
-// });
-// Route untuk menampilkan form login
-Route::get('/', [AuthController::class, 'formlogin'])->name('login');
-
-Route::post('/', [AuthController::class, 'login']);
-
-// Route untuk menampilkan form register
+// Rute untuk tampilan login dan registrasi
+Route::get('/login', [AuthController::class, 'formlogin'])->name('login');
 Route::get('/register', [AuthController::class, 'formregister'])->name('register');
-
-// Route untuk menangani register
 Route::post('/register', [AuthController::class, 'register']);
+
+// Rute untuk proses login
+Route::post('/login', [AuthController::class, 'login']);
 
